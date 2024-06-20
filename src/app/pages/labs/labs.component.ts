c58dccc98d4562c7da8dc85667c5ea95bd9cb79c
David Adriazola <d.adriazolamuriel@gmail.com>
2024-06-20 17:56:40 -0400

    step 4
    migration to angular v17

--- src/app/pages/labs/labs.component.ts
+++ src/app/pages/labs/labs.component.ts
@@ -1,10 +1,10 @@
-import { CommonModule } from '@angular/common';
+
 import { Component, signal } from '@angular/core';
 import { FormControl, ReactiveFormsModule, Validators } from '@angular/forms'
 @Component({
   selector: 'app-labs',
   standalone: true,
-  imports: [CommonModule, ReactiveFormsModule],
+  imports: [ReactiveFormsModule],
   templateUrl: './labs.component.html',
   styleUrl: './labs.component.css',
 })
