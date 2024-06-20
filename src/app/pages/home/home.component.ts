fc96493609433311fd5e9b533849471a7c161731
David Adriazola <d.adriazolamuriel@gmail.com>
2024-06-19 23:05:16 -0400

    binding

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -65,7 +65,41 @@ export class HomeComponent {
         }
         return task;
       })
-    } )
+    } );
+  }
+
+  updateTaskEditingMode(index: number){
+    this.tasks.update((tasks)=> {
+      return tasks.map((task, position)=>{
+        if(position === index){
+          return {
+            ...task,
+            editing: true
+          }
+        }
+        return {
+          ...task,
+          editing: false
+        }
+      })
+    } );
+  }
+
+  updateTaskText(index: number, event: Event){
+    const input =event.target as HTMLInputElement;
+    this.tasks.update(prevState=>{
+      return prevState.map((task, position)=>{
+        if(position === index){
+          return {
+            ...task,
+            title: input.value,
+            editing: false
+          }
+        }
+        return task;
+      }
+    )
+    });
   }
 
 }
